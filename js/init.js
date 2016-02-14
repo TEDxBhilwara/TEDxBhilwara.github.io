4d7bd9233587468dd574e98ba61d6292bcddf055
Rishabh Trivedi <1rishabhtrivedi@gmail.com>
2016-02-14 17:13:34 +0530

    mble frndly

--- js/init.js
+++ js/init.js
@@ -1,8 +1,20 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
+        $('.button-collapse').sideNav();
 
-  }); // end of document ready
+        $(document)
+            .ready(function() {
+                $('.slider').slider({
+                    full_width: true
+                });
+            });
+
+        $('.modal-trigger').leanModal({
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .5, // Opacity of modal background
+            in_duration: 300, // Transition in duration
+            out_duration: 200 // Transition out duration
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
